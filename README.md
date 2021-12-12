147a108bbb96820d6842e95ba07efec75207441c
Maor Leger <malege@microsoft.com>
2021-12-11 22:13:40 -0800

    day12

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
-![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -132,9 +132,9 @@ Both parts: 10.2ms
 
 ```
 Day 10
-Time part 1: 1.14ms
-Time part 2: 0.81ms
-Both parts: 1.95ms
+Time part 1: 1.16ms
+Time part 2: 0.8ms
+Both parts: 1.96ms
 ```
 
 ```
@@ -146,9 +146,9 @@ Both parts: 1.5899999999999999ms
 
 ```
 Day 12
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 12.46ms
+Time part 2: 215.93ms
+Both parts: 228.39000000000001ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 22/50
-Total time: 431.34ms
+Total stars: 24/50
+Total time: 659.74ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day12/index.ts
@@ -0,0 +1,74 @@
+import run from "aocrunner";
+import { EOL } from "os";
+import _ from "lodash";
+
+const parseInput = (rawInput: string) =>
+  rawInput.split(EOL).reduce((acc, row) => {
+    const [lhs, rhs] = row.split("-");
+    acc[lhs] ||= [];
+    acc[lhs].push(rhs);
+    acc[rhs] ||= [];
+    acc[rhs].push(lhs);
+    return acc;
+  }, {} as Record<string, string[]>);
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return dfs("start", input, new Set<string>(), {
+    multipleVisitsAllowed: false,
+  });
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return dfs("start", input, new Set<string>(), {
+    multipleVisitsAllowed: true,
+  });
+};
+
+function dfs(
+  node: string,
+  graph: ReturnType<typeof parseInput>,
+  seen: Set<string>,
+  rules: { multipleVisitsAllowed: boolean },
+) {
+  seen = new Set(seen);
+  seen.add(node);
+
+  if (node === "end") {
+    return 1;
+  }
+
+  let pathCount = 0;
+  for (const neighbor of graph[node]) {
+    if (neighbor === "start") {
+      continue;
+    }
+    if (seen.has(neighbor)) {
+      if (neighbor === "end" || neighbor.toUpperCase() === neighbor) {
+        // unlimited visits
+        pathCount += dfs(neighbor, graph, seen, rules);
+      } else if (rules.multipleVisitsAllowed) {
+        // Use our one double-visit pass
+        pathCount += dfs(neighbor, graph, seen, {
+          multipleVisitsAllowed: false,
+        });
+      }
+    } else {
+      pathCount += dfs(neighbor, graph, seen, rules);
+    }
+  }
+  return pathCount;
+}
+
+run({
+  part1: {
+    tests: [],
+    solution: part1,
+  },
+  part2: {
+    tests: [],
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
